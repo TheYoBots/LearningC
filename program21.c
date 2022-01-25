228d19b1b01756cd1af94a63ce580898019d0895
Yohaan Seth Nathan <73843275+TheYoBots@users.noreply.github.com>
2022-01-25 14:25:27 +0530

    Programs 19-22

---
+++ program21.c
@@ -0,0 +1,19 @@
+/*
+C program to swap two numbers.
+ii. Without using a third variable
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    int a,b,temp;
+    printf("Enter two numbers - a,b : ");
+    scanf("%d %d",&a,&b);
+    printf("\nBefore swaping a= %d and b = %d",a,b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("\nUsing a third variable after swaping a = %d and b = %d",a,b);
+    return 0;
+}
