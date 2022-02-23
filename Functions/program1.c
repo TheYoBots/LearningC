f570f51f66403e045148f4322430e8d1bb9b7960
Yohaan Seth Nathan <73843275+TheYoBots@users.noreply.github.com>
2022-02-23 21:59:44 +0530

    Functions Program 1 (basic intro)

---
+++ Functions/program1.c
@@ -0,0 +1,22 @@
+// Sum of 2 numbers using functions
+
+#include<stdio.h>
+
+int Sum(int x, int y);
+
+int main()
+{
+    int a,b,sum;
+    printf("Enter two numbers: ");
+    scanf("%d %d",&a,&b);
+    sum=Sum(a,b);
+    printf("Sum = %d",sum);
+    return 0;
+}
+
+int Sum(int x, int y)
+{
+    int sum;
+    sum=x+y;
+    return sum;
+}
